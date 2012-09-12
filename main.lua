47909aa69c274514ea51370d33b78907945f4cfc
Daniel Manchon <dmanchon@gmail.com>
2012-09-13 00:14:47 +0200

    World wrap now seems to be fine

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ end
 
 function love.draw()
    draw_ship(xpos,ypos)
- 
+   
    -- check boundaries
    w = love.graphics.getWidth()
    h = love.graphics.getHeight()
@@ -19,30 +19,29 @@ function love.draw()
    if (xpos > 0) then
       x_a = w-xpos
       x_b = ship:getImageData():getWidth()-x_a
+      if x_b > 0  then
+	 draw_ship(-x_a,ypos)
+      end
    else
-      x_a = xpos
-      x_b = ship:getImageData():getWidth()-x_a
+      x_a = -xpos
+      x_b = ship:getImageData():getWidth() - x_a
+      if x_a > 0 then
+	 draw_ship(w-x_a,ypos)
+      end
    end
 
    if (ypos > 0) then
       y_a = h-ypos
       y_b= ship:getImageData():getHeight() - y_a
+      if y_b > 0 then
+	 draw_ship(xpos,-y_a)
+      end
    else
-      y_a = ypos
+      y_a = -ypos
       y_b= ship:getImageData():getHeight() - y_a
-   end
-
-   x = xpos
-   y = ypos
-
-   if x_b > 0  then
-      draw_ship(-x_a,ypos)
-   elseif y_b > 0 then
-      draw_ship(xpos,-y_a)
-   elseif y_a < 0 then
-      draw_ship(xpos,h+y_a)
-   elseif x_a < 0 then
-      draw_ship(ypos,w+x_a)
+      if y_a > 0 then
+	 draw_ship(xpos,h-y_a)
+      end
    end
    
    -- take care of corners case
@@ -50,9 +49,13 @@ function love.draw()
       ypos = 0
    elseif xpos > w then
       xpos = 0
+   elseif (-ypos) > ship:getImageData():getHeight() then
+      ypos = h-y_a
+   elseif (-xpos) > ship:getImageData():getWidth() then
+      xpos = w-x_a
    end
 
-   love.graphics.print(tostring(love.timer.getFPS( )).."FPS, x="..tostring(xpos)..", y="..tostring(ypos), w-200,20)
+   love.graphics.print("x_a="..tostring(x_a), w-400,30)
 
 end
 
@@ -62,13 +65,13 @@ function love.update(dt)
       val = val + dt   -- we will increase the variable by 1 for every second the key is held down
    elseif  love.keyboard.isDown("down") then
       val = val - dt
-    end 
+   end 
 
    if love.keyboard.isDown("left") then
       val2 = val2 - dt   -- we will increase the variable by 1 for every second the key is held down
    elseif  love.keyboard.isDown("right") then
       val2 = val2 + dt
-    end 
+   end 
 
 
    xpos = xpos + val2
